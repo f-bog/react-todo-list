a310a02f027b5925a7aacfbc1a8f0b7a9edbe974
Fred Bogdanoff <fredericks.webdesigns@gmail.com>
2020-09-16 18:07:53 +1000

    changed input styling

--- src/TodoList.js
+++ src/TodoList.js
@@ -37,7 +37,7 @@ class TodoList extends Component {
   render() {
     return (
       <div className='TodoList'>
-        <h1>Add a Todo</h1>
+        <h1>Todo List</h1>
         <NewTodoForm addTodo={this.addTodo} />
         {this.state.todos.map((todo) => (
           <Todo
