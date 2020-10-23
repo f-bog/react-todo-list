b31eb43913e5c9e6abcd2fb714be434871cf1583
Fred Bogdanoff <fredericks.webdesigns@gmail.com>
2020-10-24 00:00:40 +1000

    Refactor NewTodoForm for hooks

--- src/NewTodoForm.js
+++ src/NewTodoForm.js
@@ -1,45 +1,37 @@
-import React, { Component } from 'react';
+import React, { useState } from 'react';
 import './NewTodoForm.css';
-const {"v4": uuidv4} = require('uuid');
-class NewTodoForm extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      task: '',
-    };
-    this.handleChange = this.handleChange.bind(this);
-    this.handleSubmit = this.handleSubmit.bind(this);
-  }
+const { v4: uuidv4 } = require('uuid');
+function NewTodoForm({ addTodo }) {
+  const [todo, setTodo] = useState({ task: '' });
 
-  handleChange(e) {
-    this.setState({ [e.target.name]: e.target.value });
-  }
+  const handleChange = e => {
+    setTodo({ task: e.target.value });
+  };
 
-  handleSubmit(e) {
+  const handleSubmit = e => {
     e.preventDefault();
-    if (this.state.task === '') {
+    if (todo.task === '') {
       return;
     }
-    this.props.addTodo({ ...this.state, id: uuidv4(), completed: false }); // pass data to parent
-    this.setState({ task: '', id: null });
-  }
+    addTodo({ task: todo.task, id: uuidv4(), completed: false }); // pass data to parent
+    setTodo({ task: '', id: null });
+  };
 
-  render() {
-    return (
-      <form className='NewTodoForm' onSubmit={this.handleSubmit}>
-        <label htmlFor='task'>Add a todo</label>
-        <br />
-        <input
-          id='task'
-          value={this.state.task}
-          name='task'
-          type='text'
-          placeholder='Add a todo'
-          onChange={this.handleChange}
-        ></input>
-        <button>New Todo</button>
-      </form>
-    );
-  }
+  return (
+    <form className='NewTodoForm' onSubmit={handleSubmit}>
+      <label htmlFor='task'>Add a todo</label>
+      <br />
+      <input
+        id='task'
+        value={todo.task}
+        name='task'
+        type='text'
+        placeholder='Add a todo'
+        onChange={handleChange}
+      ></input>
+      <button>New Todo</button>
+    </form>
+  );
 }
+
 export default NewTodoForm;
